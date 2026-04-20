24cdeb41b099d11426cd77e47e10a51a502ff873
aarnioem <aarnioem@gmail.com>
2026-04-20 06:47:43 +0300

    eq method for testing

--- src/core/melds.py
+++ src/core/melds.py
@@ -6,3 +6,15 @@ class Meld:
         self.from_player = from_player
         self.meld_type = meld_type
         self.open_call = open_call
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Meld):
+            return NotImplemented
+
+        return (
+            self.called_tile == value.called_tile
+            and self.tiles == value.tiles
+            and self.from_player == value.from_player
+            and self.meld_type == value.meld_type
+            and self.open_call == value.open_call
+        )
