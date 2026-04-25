1e38123562298dd9f4f17342c880ef8fe8c15f67
aarnioem <aarnioem@gmail.com>
2026-04-25 20:11:44 +0300

    rinshan functionality for the wall class

--- src/core/wall.py
+++ src/core/wall.py
@@ -23,22 +23,57 @@ class Wall:
 
         self.total_tiles = len(self.tiles)
         self.dead_wall_size = dead_wall_size
+        self.kan_counter = 0
 
         if shuffle:
             random.shuffle(self.tiles)
 
+        self.rinshan_tiles = self._get_rinshan_tiles()
+
         self.draw_pointer = 0
 
     def live_tiles(self):
+        """Returns the number of drawable tiles in the wall
+
+        Returns:
+            int: tiles left
+        """
         return self.total_tiles - self.draw_pointer - self.dead_wall_size
 
     def draw_tile(self):
-        """Draws one tile from the wall.
-        Returns the tile ID and increments the draw pointer"""
+        """Draws a tile from the live wall and increments the draw counter
+        Returns:
+            int: tile id"""
 
         if self.live_tiles() <= 0:
             raise IndexError("No tiles left in the wall")
 
         tile = self.tiles[self.draw_pointer] # add some error checking here
         self.draw_pointer += 1
+        return tile
+
+    def _get_rinshan_tiles(self):
+        """Sets up the 4 rinshan tiles in correct order for convenience
+
+        Returns:
+            list: rinshan tiles
+        """
+        return [
+            self.tiles[-2],
+            self.tiles[-1],
+            self.tiles[-4],
+            self.tiles[-3]
+        ]
+
+
+    def draw_rinshan_tile(self):
+        """Draws a tile from the dead wall and increments the kan counter
+        Returns:
+            int: tile id
+        """
+        if self.live_tiles() <= 0:
+            raise IndexError("No tiles left in the wall")
+
+        tile = self.rinshan_tiles[self.kan_counter]
+        self.kan_counter += 1
         return tile
